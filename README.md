56c7eaa88312bbcf997a940196ff9fbf830e5136
Ben Christel <benchristel@khanacademy.org>
2023-06-15 14:33:35 -0700

    Add minimal repro

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # MathJax memory leak repro
 
-When rendering math with `mathjax-full@3.2.2`, MathJax leaks MML nodes.
+When rendering math with `mathjax-full@3.2.2` and the `textmacros` package, MathJax leaks memory.
+
+To repro the issue:
+
+```
+npm install # or yarn install
+npm run dev # or yarn dev
+```
+
+Open http://127.0.0.1:5173/ in two Google Chrome tabs.
+
+Click "Start tests without textmacros" in one, and "Start tests with textmacros" in the other.
+
+Watch the "Memory" devtools tab. When rendering without textmacros, memory usage stays below 20MB.
+When rendering with textmacros, memory usage quickly exceeds 20MB and continues climbing.
--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("Hello, world!")
+import {mathjax} from "mathjax-full/js/mathjax.js";
+import {TeX} from "mathjax-full/js/input/tex.js";
+import {CHTML} from "mathjax-full/js/output/chtml.js";
+import {browserAdaptor} from "mathjax-full/js/adaptors/browserAdaptor.js";
+import {RegisterHTMLHandler} from "mathjax-full/js/handlers/html.js";
+
+// MathJax Packages
+import "mathjax-full/js/input/tex/textmacros/TextMacrosConfiguration.js";
+
+function startTest(shouldUseTextmacros) {
+    RegisterHTMLHandler(browserAdaptor());
+
+    const chtmlOutput = new CHTML({})
+    const texInput = new TeX({
+        packages: [
+            "base",
+            ...(shouldUseTextmacros ? ["textmacros"] : []),
+        ],
+        formatError(jax, err) {
+            return jax.formatError(err);
+        },
+    })
+
+    const doc = mathjax.document("", {
+        InputJax: texInput,
+        OutputJax: chtmlOutput,
+    });
+
+    setInterval(() => {
+        doc.convert("\\text{yadda yadda yadda}", {display: false})
+        console.log(`rendered ${shouldUseTextmacros ? "with" : "without"} textmacros`);
+    }, 1)
+}
+
+document.getElementById("start-with-textmacros")
+    .addEventListener("click", () => startTest(true))
+document.getElementById("start-without-textmacros")
+    .addEventListener("click", () => startTest(false))
